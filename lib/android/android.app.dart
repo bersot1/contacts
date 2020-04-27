ad8748ea4816310e8b5e98d9b5442bc1a4f4fdfe
Lucas Bersot Marinho <lucas.marinho1@vale.com>
2020-04-27 11:08:40 -0300

    First commit

---
+++ lib/android/android.app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AndroidApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Contacts',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Text("OK"),
+      ),
+    );
+  }
+}
---
+++ lib/android/styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const brightness = Brightness.dark;
+const primaryColor = const Color(0xFF2AACFF);
+const accentColor = const Color(0xFFFFFFFF);
+
+ThemeData androidTheme() {
+  return ThemeData(
+    brightness: brightness,
+  );
+}
